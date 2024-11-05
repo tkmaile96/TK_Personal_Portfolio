c48882dc7c795bbf2fc48ceaa6d59c6d510a30c4
Teekay <tkmaile96@gmail.com>
2024-11-05 13:53:22 +0200

    Updated index.css, index.html, and index.js files with new styles, HTML structure, and JavaScript functionality for the introduction section and profile user interface.

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ function hideSidebar(){
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "none";
 }
+
+//Profile-user-status
+
+const status = ["Web Developer",  "Freelancer", "Full Stack Developer", "Web Designer", "UI/UX Designer"];
+let index = 0;
+
+function changeStatus(){
+   const messages = document.getElementById('messages') ;
+   const statusSpan = document.querySelector('status-span');
+   messages.textContent = `I am a ${status[index]}`
+   index = (index + 1) % status.length ;
+
+   setTimeout(() => {
+    messages.textContent = 'I am a';
+   }, 2000)
+
+   setTimeout(changeStatus, 3000);
+}
+changeStatus();
+
+
