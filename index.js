d955bfb35ad12255b76cf92334bae45b7c8edc5f
Teekay <tkmaile96@gmail.com>
2024-11-08 18:36:49 +0200

    Updated CSS styles, added new sections for projects and contacts, modified HTML structure, and removed JavaScript code for profile user status animation

--- index.js
+++ index.js
@@ -7,23 +7,6 @@ function hideSidebar(){
     sidebar.style.display = "none";
 }
 
-//Profile-user-status
 
-const status = ["Web Developer",  "Freelancer", "Full Stack Developer", "Web Designer", "UI/UX Designer"];
-let index = 0;
-
-function changeStatus(){
-   const messages = document.getElementById('messages') ;
-   const statusSpan = document.querySelector('status-span');
-   messages.textContent = `I am a ${status[index]}`
-   index = (index + 1) % status.length ;
-
-   setTimeout(() => {
-    messages.textContent = 'I am a';
-   }, 2000)
-
-   setTimeout(changeStatus, 3000);
-}
-changeStatus();
 
 
